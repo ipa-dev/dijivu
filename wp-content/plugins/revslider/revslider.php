9b4f019aa219471f8b94f0f70b1a85ef685a61aa
Mason Watson <mkw@Masons-MacBook-Pro.local>
2018-08-27 13:32:14 -0400

    add new revslider

--- wp-content/plugins/revslider/revslider.php
+++ wp-content/plugins/revslider/revslider.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Revolution Slider (Shared on Themestotal.com)
-Plugin URI: http://www.themepunch.com/revolution/
-Description: Revolution Slider - Premium responsive slider
+Plugin Name: Slider Revolution
+Plugin URI: https://revolution.themepunch.com/
+Description: Slider Revolution - Premium responsive slider
 Author: ThemePunch
-Version: 4.6.5
-Author URI: http://themepunch.com
+Version: 5.4.8
+Author URI: https://themepunch.com
 */
 
 // If this file is called directly, abort.
@@ -14,8 +14,25 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 if(class_exists('RevSliderFront')) {
-	die('ERROR: It looks like you have more than one instance of Revolution Slider installed. Please remove additional instances for this plugin to work again.');
+	die('ERROR: It looks like you have more than one instance of Slider Revolution installed. Please remove additional instances for this plugin to work again.');
 }
+
+$revSliderVersion	= '5.4.8';
+$revSliderAsTheme	= false;
+$revslider_screens	= array();
+$revslider_fonts	= array();
+$rs_plugin_url		= str_replace('index.php','',plugins_url( 'index.php', __FILE__ ));
+if(strpos($rs_plugin_url, 'http') === false) {
+	$site_url		= get_site_url();
+	$rs_plugin_url	= (substr($site_url, -1) === '/') ? substr($site_url, 0, -1). $rs_plugin_url : $site_url. $rs_plugin_url;
+}
+$rs_plugin_url		= str_replace(array(chr(10), chr(13)), '', $rs_plugin_url);
+
+define('RS_PLUGIN_PATH',		plugin_dir_path(__FILE__));
+define('RS_PLUGIN_FILE_PATH',	__FILE__);
+define('RS_PLUGIN_URL',			$rs_plugin_url);
+define('RS_PLUGIN_SLUG',		apply_filters('set_revslider_slug', 'revslider'));
+define('RS_DEMO',				false);
 
 if(isset($_GET['revSliderAsTheme'])){
 	if($_GET['revSliderAsTheme'] == 'true'){
@@ -24,12 +41,6 @@ if(isset($_GET['revSliderAsTheme'])){
 		update_option('revSliderAsTheme', 'false');
 	}
 }
-
-
-$revSliderVersion = "4.6.5";
-$currentFile = __FILE__;
-$currentFolder = dirname($currentFile);
-$revSliderAsTheme = false;
 
 //set the RevSlider Plugin as a Theme. This hides the activation notice and the activation area in the Slider Overview
 function set_revslider_as_theme(){
@@ -45,68 +56,82 @@ function set_revslider_as_theme(){
 }
 
 //include frameword files
-require_once $currentFolder . '/inc_php/framework/include_framework.php';
+require_once(RS_PLUGIN_PATH . 'includes/framework/include-framework.php');
 
 //include bases
-require_once $folderIncludes . 'base.class.php';
-require_once $folderIncludes . 'elements_base.class.php';
-require_once $folderIncludes . 'base_admin.class.php';
-require_once $folderIncludes . 'base_front.class.php';
+require_once($folderIncludes . 'base.class.php');
+require_once($folderIncludes . 'elements-base.class.php');
+require_once($folderIncludes . 'base-admin.class.php');
+require_once($folderIncludes . 'base-front.class.php');
 
 //include product files
-require_once $currentFolder . '/inc_php/revslider_settings_product.class.php';
-require_once $currentFolder . '/inc_php/revslider_globals.class.php';
-require_once $currentFolder . '/inc_php/revslider_operations.class.php';
-require_once $currentFolder . '/inc_php/revslider_slider.class.php';
-require_once $currentFolder . '/inc_php/revslider_output.class.php';
-require_once $currentFolder . '/inc_php/revslider_slide.class.php';
-require_once $currentFolder . '/inc_php/revslider_widget.class.php';
-require_once $currentFolder . '/inc_php/revslider_params.class.php';
+require_once(RS_PLUGIN_PATH . 'includes/globals.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/operations.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/slider.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/output.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/slide.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/widget.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/navigation.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/object-library.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/template.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/external-sources.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/page-template.class.php');
 
-require_once $currentFolder . '/inc_php/revslider_tinybox.class.php';
-
-require_once $currentFolder . '/inc_php/fonts.class.php'; //punchfonts
-
-require_once $currentFolder . '/inc_php/extension.class.php';
-
+require_once(RS_PLUGIN_PATH . 'includes/tinybox.class.php');
+require_once(RS_PLUGIN_PATH . 'includes/extension.class.php');
+require_once(RS_PLUGIN_PATH . 'public/revslider-front.class.php');
 
 try{
+	$rs_rsl	= (isset($_GET['rs_refresh_server'])) ? true : false;
+	$rslb	= new RevSliderLoadBalancer();
+	$GLOBALS['rslb'] = $rslb;
+	$rslb->refresh_server_list($rs_rsl);
 	
 	//register the revolution slider widget
-	UniteFunctionsWPRev::registerWidget("RevSlider_Widget");
+	RevSliderFunctionsWP::registerWidget('RevSliderWidget');
 
 	//add shortcode
-	function rev_slider_shortcode($args){
-
+	function rev_slider_shortcode($args, $mid_content = null){
+		
         extract(shortcode_atts(array('alias' => ''), $args, 'rev_slider'));
-        $sliderAlias = ($alias != '') ? $alias : UniteFunctionsRev::getVal($args,0);
+		extract(shortcode_atts(array('settings' => ''), $args, 'rev_slider'));
+		extract(shortcode_atts(array('order' => ''), $args, 'rev_slider'));
+		
+		if($settings !== '') $settings = json_decode(str_replace(array('({', '})', "'"), array('[', ']', '"'), $settings) ,true);
+		if($order !== '') $order = explode(',', $order);
+		
+        $sliderAlias = ($alias != '') ? $alias : RevSliderFunctions::getVal($args,0);
+		
+		$gal_ids = RevSliderFunctionsWP::check_for_shortcodes($mid_content); //check for example on gallery shortcode and do stuff
 		
 		ob_start();
-		$slider = RevSliderOutput::putSlider($sliderAlias);
+		if(!empty($gal_ids)){ //add a gallery based slider
+			$slider = RevSliderOutput::putSlider($sliderAlias, '', $gal_ids);
+		}else{
+			$slider = RevSliderOutput::putSlider($sliderAlias, '', array(), $settings, $order);
+		}
 		$content = ob_get_contents();
 		ob_clean();
 		ob_end_clean();
 		
-		// Do not output Slider if we are on mobile
-		$disable_on_mobile = $slider->getParam("disable_on_mobile","off");
-		if($disable_on_mobile == 'on'){
-			$mobile = (strstr($_SERVER['HTTP_USER_AGENT'],'Android') || strstr($_SERVER['HTTP_USER_AGENT'],'webOS') || strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') ||strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || wp_is_mobile()) ? true : false;
-			
-			if($mobile)
-				return false;
-		}
-		
-		$show_alternate = $slider->getParam("show_alternative_type","off");
-		
-		if($show_alternate == 'mobile' || $show_alternate == 'mobile-ie8'){
-			if(wp_is_mobile()){
-				$show_alternate_image = $slider->getParam("show_alternate_image","");
-				return '<img class="tp-slider-alternative-image" src="'.$show_alternate_image.'">';
-			}
-		}
-		
-		//handle slider output types
 		if(!empty($slider)){
+			// Do not output Slider if we are on mobile
+			$disable_on_mobile = $slider->getParam("disable_on_mobile","off");
+			if($disable_on_mobile == 'on'){
+				$mobile = (strstr($_SERVER['HTTP_USER_AGENT'],'Android') || strstr($_SERVER['HTTP_USER_AGENT'],'webOS') || strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') ||strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || strstr($_SERVER['HTTP_USER_AGENT'],'Windows Phone') || wp_is_mobile()) ? true : false;
+				if($mobile) return false;
+			}
+			
+			$show_alternate = $slider->getParam("show_alternative_type","off");
+			
+			if($show_alternate == 'mobile' || $show_alternate == 'mobile-ie8'){
+				if(strstr($_SERVER['HTTP_USER_AGENT'],'Android') || strstr($_SERVER['HTTP_USER_AGENT'],'webOS') || strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') ||strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || strstr($_SERVER['HTTP_USER_AGENT'],'Windows Phone') || wp_is_mobile()){
+					$show_alternate_image = $slider->getParam("show_alternate_image","");
+					return '<img class="tp-slider-alternative-image" src="'.$show_alternate_image.'" data-no-retina>';
+				}
+			}
+		
+			//handle slider output types
 			$outputType = $slider->getParam("output_type","");
 			switch($outputType){
 				case "compress":
@@ -115,37 +140,40 @@ try{
 					return($content);
 				break;
 				case "echo":
-					echo $content;		//bypass the filters
+					echo $content; //bypass the filters
 				break;
 				default:
 					return($content);
 				break;
 			}
 		}else
-			return($content);		//normal output
+			return($content); //normal output
 
 	}
 
 	add_shortcode( 'rev_slider', 'rev_slider_shortcode' );
-
-	//add tiny box dropdown menu
-	$tinybox = new RevSlider_TinyBox();
-	
 	
 	/**
 	 * Call Extensions
 	 */
 	$revext = new RevSliderExtension();
 	
-	if(is_admin()){		//load admin part
+	add_action('plugins_loaded', array( 'RevSliderTinyBox', 'visual_composer_include' )); //VC functionality
+	add_action('plugins_loaded', array( 'RevSliderPageTemplate', 'get_instance' ));
 	
-		require_once $currentFolder . '/inc_php/framework/update.class.php';
+	if(is_admin()){ //load admin part
+	
+		require_once(RS_PLUGIN_PATH . 'includes/framework/update.class.php');
+		require_once(RS_PLUGIN_PATH . 'includes/framework/newsletter.class.php');
+		require_once(RS_PLUGIN_PATH . 'admin/revslider-admin.class.php');
 
-		require_once $currentFolder."/revslider_admin.php";
-
-		$productAdmin = new RevSliderAdmin($currentFile);
+		$productAdmin = new RevSliderAdmin(RS_PLUGIN_FILE_PATH);
 		
-	}else{		//load front part
+		//add tiny box dropdown menu
+		add_action('admin_head', array('RevSliderTinyBox', 'add_tinymce_editor'));
+		
+		
+	}else{ //load front part
 
 		/**
 		 *
@@ -153,21 +181,35 @@ try{
 		 * the data can be slider ID or slider alias.
 		 */
 		function putRevSlider($data,$putIn = ""){
-			$operations = new RevOperations();
+			$operations = new RevSliderOperations();
 			$arrValues = $operations->getGeneralSettingsValues();
-			$includesGlobally = UniteFunctionsRev::getVal($arrValues, "includes_globally","on");
-			$strPutIn = UniteFunctionsRev::getVal($arrValues, "pages_for_includes");
+			$includesGlobally = RevSliderFunctions::getVal($arrValues, "includes_globally","on");
+			$strPutIn = RevSliderFunctions::getVal($arrValues, "pages_for_includes");
 			$isPutIn = RevSliderOutput::isPutIn($strPutIn,true);
-
 			if($isPutIn == false && $includesGlobally == "off"){
 				$output = new RevSliderOutput();
-				$option1Name = "Include RevSlider libraries globally (all pages/posts)";
-				$option2Name = "Pages to include RevSlider libraries";
-				$output->putErrorMessage(__("If you want to use the PHP function \"putRevSlider\" in your code please make sure to check \" ",REVSLIDER_TEXTDOMAIN).$option1Name.__(" \" in the backend's \"General Settings\" (top right panel). <br> <br> Or add the current page to the \"",REVSLIDER_TEXTDOMAIN).$option2Name.__("\" option box."));
+				$option1Name = __("Include RevSlider libraries globally (all pages/posts)", 'revslider');
+				$option2Name = __("Pages to include RevSlider libraries", 'revslider');
+				$output->putErrorMessage(__("If you want to use the PHP function \"putRevSlider\" in your code please make sure to check \" ",'revslider').$option1Name.__(" \" in the backend's \"General Settings\" (top right panel). <br> <br> Or add the current page to the \"",'revslider').$option2Name.__("\" option box.", 'revslider'));
 				return(false);
 			}
-
-			RevSliderOutput::putSlider($data,$putIn);
+			
+			
+			ob_start();
+			$slider = RevSliderOutput::putSlider($data,$putIn);
+			$content = ob_get_contents();
+			ob_clean();
+			ob_end_clean();
+			
+			if(is_object($slider)){
+				$disable_on_mobile = @$slider->getParam("disable_on_mobile","off"); // Do not output Slider if we are on mobile
+				if($disable_on_mobile == 'on'){
+					$mobile = (strstr($_SERVER['HTTP_USER_AGENT'],'Android') || strstr($_SERVER['HTTP_USER_AGENT'],'webOS') || strstr($_SERVER['HTTP_USER_AGENT'],'iPhone') ||strstr($_SERVER['HTTP_USER_AGENT'],'iPod') || strstr($_SERVER['HTTP_USER_AGENT'],'Windows Phone') || strstr($_SERVER['HTTP_USER_AGENT'],'iPad') || wp_is_mobile()) ? true : false;
+					if($mobile) return false;
+				}
+			}
+			
+			echo $content;
 		}
 
 
@@ -181,15 +223,16 @@ try{
             return $rev->isAliasExists($alias);
 		}
 
-		require_once $currentFolder."/revslider_front.php";
-		$productFront = new RevSliderFront($currentFile);
+		$productFront = new RevSliderFront(RS_PLUGIN_FILE_PATH);
 	}
 	
+	add_action('plugins_loaded', array( 'RevSliderFront', 'createDBTables' )); //add update checks
+	add_action('plugins_loaded', array( 'RevSliderPluginUpdate', 'do_update_checks' )); //add update checks
 	
 }catch(Exception $e){
 	$message = $e->getMessage();
 	$trace = $e->getTraceAsString();
-	echo _e("Revolution Slider Error:",REVSLIDER_TEXTDOMAIN)."<b>".$message."</b>";
+	echo _e("Revolution Slider Error:",'revslider')." <b>".$message."</b>";
 }
 
 ?>
--- wp-content/plugins/revslider/uninstall.php
+++ wp-content/plugins/revslider/uninstall.php
@@ -2,17 +2,21 @@
 if( !defined( 'ABSPATH') && !defined('WP_UNINSTALL_PLUGIN') )
 	exit();
 
+/**
+ * disable deletion of anything 
+ * @since 5.0
+ 
 $currentFile = __FILE__;
 $currentFolder = dirname($currentFile);
-require_once $currentFolder . '/inc_php/revslider_globals.class.php';
+require_once $currentFolder . '/inc_php/globals.class.php';
 
 global $wpdb;
-$tableSliders = $wpdb->prefix . GlobalsRevSlider::TABLE_SLIDERS_NAME;
-$tableSlides = $wpdb->prefix . GlobalsRevSlider::TABLE_SLIDES_NAME;
-$tableSettings = $wpdb->prefix . GlobalsRevSlider::TABLE_SETTINGS_NAME;
-$tableCss = $wpdb->prefix . GlobalsRevSlider::TABLE_CSS_NAME;
-$tableAnims = $wpdb->prefix . GlobalsRevSlider::TABLE_LAYER_ANIMS_NAME;
-$tableStaticSlides = $wpdb->prefix . GlobalsRevSlider::TABLE_STATIC_SLIDES_NAME;
+$tableSliders = $wpdb->prefix . RevSliderGlobals::TABLE_SLIDERS_NAME;
+$tableSlides = $wpdb->prefix . RevSliderGlobals::TABLE_SLIDES_NAME;
+$tableSettings = $wpdb->prefix . RevSliderGlobals::TABLE_SETTINGS_NAME;
+$tableCss = $wpdb->prefix . RevSliderGlobals::TABLE_CSS_NAME;
+$tableAnims = $wpdb->prefix . RevSliderGlobals::TABLE_LAYER_ANIMS_NAME;
+$tableStaticSlides = $wpdb->prefix . RevSliderGlobals::TABLE_STATIC_SLIDES_NAME;
 
 $wpdb->query( "DROP TABLE $tableSliders" );
 $wpdb->query( "DROP TABLE $tableSlides" );
@@ -21,14 +25,19 @@ $wpdb->query( "DROP TABLE $tableCss" );
 $wpdb->query( "DROP TABLE $tableAnims" );
 $wpdb->query( "DROP TABLE $tableStaticSlides" );
 
+//deactivate activation if plugin was activated
+
 delete_option('revslider-latest-version');
 delete_option('revslider-update-check-short');
 delete_option('revslider-update-check');
 delete_option('revslider_update_info');
-delete_option('revslider-api-key');
-delete_option('revslider-username');
 delete_option('revslider-code');
 delete_option('revslider-valid');
 delete_option('revslider-valid-notice');
+*/
 
+//needs to be deleted so that everything gets checked at a new installation
+delete_option('revslider_table_version');
+delete_option('revslider_checktables');
+delete_option('rs_public_version');
 ?>
